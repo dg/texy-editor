377c0a0db4e9239fa3324b3057d5867011b44e1f
David Grudl <david@grudl.com>
2011-03-07 14:44:24 +0100

    support for multiline tabs (only non-IE browsers)

--- index.php
+++ index.php
@@ -65,12 +65,21 @@ if (isset($_POST['text'])) { // vystup pro AJAX
 				});
 			}, 400);
 
-			if (e.which == 9 /* TAB */ && !e.shiftKey && !e.ctrlKey && !e.altKey) {
+			if (e.which == 9 /* TAB */ && !e.ctrlKey && !e.altKey) {
 				if (e.target.setSelectionRange) { // non-IE
-					var start = e.target.selectionStart;
+					var start = e.target.selectionStart, end = e.target.selectionEnd;
 					var top = e.target.scrollTop;
-					e.target.value = e.target.value.substring(0, start) + "\t" + e.target.value.substr(e.target.selectionEnd);
-					e.target.setSelectionRange(start + 1, start + 1);
+					if (start !== end) {
+						start = e.target.value.lastIndexOf("\n", start) + 1;
+					}
+					var sel = e.target.value.substring(start, end);
+					if (e.shiftKey) {
+						sel = sel.replace(/^\t/gm, '');
+					} else {
+						sel = sel.replace(/^/gm, "\t");
+					}
+					e.target.value = e.target.value.substring(0, start) + sel + e.target.value.substr(end);
+					e.target.setSelectionRange(start === end ? start + 1 : start, start + sel.length);
 					e.target.focus();
 					e.target.scrollTop = top; // Firefox
 
